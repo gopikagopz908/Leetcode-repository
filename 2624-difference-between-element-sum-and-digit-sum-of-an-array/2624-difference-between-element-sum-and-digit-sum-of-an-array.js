61d66aa0d8241c109cf72a4946be07df8adea574
Gopika c <156649539+gopikagopz908@users.noreply.github.com>
2025-01-13 09:49:18 +0530

    Time: 31 ms (10.46%), Space: 55.2 MB (22.87%) - LeetHub

--- 2624-difference-between-element-sum-and-digit-sum-of-an-array/2624-difference-between-element-sum-and-digit-sum-of-an-array.js
+++ 2624-difference-between-element-sum-and-digit-sum-of-an-array/2624-difference-between-element-sum-and-digit-sum-of-an-array.js
@@ -5,9 +5,9 @@
 var differenceOfSum = function(nums) {
 
     let elementsum=nums.reduce((acc,curr)=>acc+curr,0)
-    let digitsum=nums.map((item)=>item.toString().split(''))
-    let a=digitsum.flat()
-    let sum=a.reduce((a,b)=>Number(a)+Number(b),0)
+    let digitsum=nums.map((item)=>item.toString().split('')).flat()
+    // let a=digitsum.flat()
+    let sum=digitsum.reduce((a,b)=>Number(a)+Number(b),0)
     return elementsum-sum
 
     
