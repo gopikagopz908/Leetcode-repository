ea8fd54b53ab4370bd734ccb4ba529a471445ae3
Gopika c <156649539+gopikagopz908@users.noreply.github.com>
2025-01-23 10:30:05 +0530

    Time: 6 ms (75.68%), Space: 55 MB (36.77%) - LeetHub

---
+++ 2502-sort-the-people/2502-sort-the-people.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string[]} names
+ * @param {number[]} heights
+ * @return {string[]}
+ */
+var sortPeople = function(names, heights) {
+
+   const people = names.map((name, index) => ({ name, height: heights[index] }));
+    
+    
+    people.sort((a, b) => b.height - a.height);
+    
+    
+    return people.map(person => person.name);
+
+    
+    
+};
