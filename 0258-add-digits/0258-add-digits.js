478f83959701dcca0bbaa741a1678e56351ca65f
Gopika c <156649539+gopikagopz908@users.noreply.github.com>
2025-04-12 16:41:26 +0530

    Time: 0 ms (100%), Space: 56.7 MB (63.14%) - LeetHub

---
+++ 0258-add-digits/0258-add-digits.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} num
+ * @return {number}
+ */
+var addDigits = function(num) {
+   if(num==0){
+    return 0
+   } 
+   if(num>0 && num%9==0){
+    return 9
+   }else{
+    return num%9
+   }
+
+};
